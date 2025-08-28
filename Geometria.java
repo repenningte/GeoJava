c7daef355effab7d4e75fd41ad623af7c089b11e
Teresa Repenning <134066092+repenningte@users.noreply.github.com>
2025-08-28 12:08:59 -0300

    .

--- Geometria.java
+++ Geometria.java
@@ -19,7 +19,6 @@ public class Geometria {
                 System.out.println(objEsfera.volume);
         }
 
-
         public static void Cone(){
                 GeometriaEspacial objCone = new GeometriaEspacial();
                 Scanner ler = new Scanner(System.in);
@@ -33,9 +32,79 @@ public class Geometria {
                 System.out.println(objCone.areaBase);
                 System.out.println(objCone.areaLateral);
                 System.out.println(objCone.volume);
-                 
-                
         }
+
+        public static void Cilindro(){
+                GeometriaEspacial objCilindro = new GeometriaEspacial();
+                Scanner ler = new Scanner(System.in);
+                System.out.println("Digite a altura do Cilindro: ");
+                objCilindro.h = ler.nextDouble();
+                objCilindro.Cilindro();
+                System.out.println(objCilindro.areaLateral);
+                System.out.println(objCilindro.areaBase);
+                System.out.println(objCilindro.volume);
+        }
+
+        public static void TroncoDeCone(){
+                GeometriaEspacial objTroncoCone = new GeometriaEspacial();
+                Scanner ler = new Scanner(System.in);
+                System.out.println("Digite a altura do Tronco de Cone: ");
+                System.out.println("Digite o raio do Tronco de Cone: ");
+                System.out.println("Digite o raio maior Tronco de Cone: ");
+                objTroncoCone.R = ler.nextDouble();
+                objTroncoCone.r = ler.nextDouble();
+                objTroncoCone.h = ler.nextDouble();
+                objTroncoCone.TroncoDeCone();
+                System.out.println(objTroncoCone.areaBaseMaiorTronco);
+                System.out.println(objTroncoCone.areaBaseMenorTronco);
+                System.out.println(objTroncoCone.areaBaseTronco);
+                System.out.println(objTroncoCone.areaLateralTronco);
+                System.out.println(objTroncoCone.areaTotalTronco);
+                System.out.println(objTroncoCone.volumeTronco);
+        }
+
+        public static void TroncoDePiramide(){
+                GeometriaEspacial objTroncoPiramide = new GeometriaEspacial();
+                Scanner ler = new Scanner(System.in);
+                System.out.println("Digite a altura do Tronco da Piramide: ");
+                System.out.println("Digite o raio do Tronco da Piramide: ");
+                System.out.println("Digite o raio maior Tronco ds Piramide: ");
+                objTroncoPiramide.R = ler.nextDouble();
+                objTroncoPiramide.r = ler.nextDouble();
+                objTroncoPiramide.h = ler.nextDouble();
+                objTroncoPiramide.TroncoDePiramide();
+                System.out.println(objTroncoPiramide.areaBaseMaiorTronco);
+                System.out.println(objTroncoPiramide.areaBaseMenorTronco);
+                System.out.println(objTroncoPiramide.areaBaseTronco);
+                System.out.println(objTroncoPiramide.areaLateralTronco);
+                System.out.println(objTroncoPiramide.areaTotalTronco);
+                System.out.println(objTroncoPiramide.volumeTronco);
+        }
+
+        public static void Piramide(){
+                GeometriaEspacial objPiramide = new GeometriaEspacial();
+                Scanner ler = new Scanner(System.in);
+                System.out.println("Digite o valor da base da piramide: ");
+                System.out.println("Digite a apótema da piramide: ");
+                objPiramide.b = ler.nextDouble();
+                objPiramide.ap = ler.nextDouble();
+                objPiramide.Piramide();
+                System.out.println(objPiramide.areaLateralPiramide);
+                System.out.println(objPiramide.areaTotalPiramide);
+                System.out.println(objPiramide.volumePiramide);
+        }
+
+        public static void Prisma(){
+                GeometriaEspacial objPrisma = new GeometriaEspacial();
+                Scanner ler = new Scanner(System.in);
+                System.out.println("Digite o perímetro da base do prisma: ");
+                objPrisma.P = ler.nextDouble();
+                objPrisma.Prisma();
+                System.out.println(objPrisma.areaLateralPrisma);
+                System.out.println(objPrisma.areaTotalPrismas);
+                System.out.println(objPrisma.volumePrisma);
+        }
+
 
         
 
